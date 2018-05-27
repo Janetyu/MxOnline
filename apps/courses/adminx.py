68830f3d4d87d8ed4ca85b6dd923cc74a77141cc
Janetyu <931242644@qq.com>
2018-05-27 23:29:25 +0800

    添加其他文件进入仓库

--- apps/courses/adminx.py
+++ apps/courses/adminx.py
@@ -11,6 +11,7 @@ class CourseAdmin(object):
     list_display = ['name','desc','detail','degree','learn_times','students','fav_nums','image','click_nums','add_time']
     search_fields = ['name','desc','detail','degree','learn_times','students','fav_nums','image','click_nums']
     list_filter = ['name','desc','detail','degree','learn_times','students','fav_nums','image','click_nums','add_time']
+    i = 0
 
 
 class LessonAdmin(object):
