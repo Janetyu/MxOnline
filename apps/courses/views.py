5078fa0879d2872daebfc311aa93118c711573a8
Janetyu <931242644@qq.com>
2018-07-16 00:35:36 +0800

    添加CourseCommentView的该课同学还学过的逻辑

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -116,12 +116,30 @@ class CourseCommentView(LoginRequiredMixin, View):
     """
     def get(self, request, course_id):
         course = Course.objects.get(id=int(course_id))
+
+        # 查询用户是否已经关联了该课程
+        user_courses = UserCourse.objects.filter(user=request.user, course=course)
+        if not user_courses:
+            user_course = UserCourse(user=request.user, course=course)
+            user_course.save()
+
+        # 获取所有课程相关的用户
+        user_courses = UserCourse.objects.filter(course=course)
+        user_ids = [user_course.user.id for user_course in user_courses]
+        # 取出该用户的所有课程
+        all_user_courses = UserCourse.objects.filter(user_id__in=user_ids)
+        # 取出所有课程id
+        course_ids = [user_course.course.id for user_course in all_user_courses]
+        # 获取学过该课程的用户学过其他的所有课程
+        relate_courses = Course.objects.filter(id__in=course_ids).order_by("-click_nums")[:5]
+
         all_resources = CourseResource.objects.filter(course=course)
         all_comments = CourseComments.objects.filter(course=course)
         return render(request, "course-comment.html", {
             "course": course,
             "course_resources": all_resources,
             "all_comments": all_comments,
+            "relate_courses": relate_courses
         })
 
 
